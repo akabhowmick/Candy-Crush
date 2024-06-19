284669b698f953710b19dea8452e084b05ad5f95
Akash Bhowmick <akabhowmick@gmail.com>
2024-06-18 22:38:39 -0400

    add border styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 export const Header = () => {
   return (
     <div>
-      <h1>Candy Crush!</h1>
-      <h3>Can you get a 100 points in 1 minute?</h3>
+      <h1 className="site-title">Candy Crush!</h1>
+      <h3 className="site-subheader">Can you get a 100 points in 1 minute?</h3>
       <h3>Get your highest score</h3>
     </div>
   );
